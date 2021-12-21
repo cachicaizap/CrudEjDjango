0d10e212da11a36ace3fe828c30576c7764f3fec
cachicaizap <68747922+cachicaizap@users.noreply.github.com>
2021-12-21 12:27:44 -0500

    Version 1.2.1

--- gestionClientes/models.py
+++ gestionClientes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Clientes(models.Model):
+    nombre=models.CharField(max_length=30)
+    direccion= models.CharField(max_length=50)
+    email=models.EmailField()
+    tfno=models.CharField(max_length=10)
